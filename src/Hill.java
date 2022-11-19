1059ee01f20d51d8d32e052e8a489607f8ea8e06
trueeh-slave <steamtruesnowace@gmail.com>
2022-11-19 17:07:25 -0500

    intento de decodificacion xd

--- src/Hill.java
+++ src/Hill.java
@@ -1,6 +1,8 @@
 import org.apache.commons.math3.linear.Array2DRowRealMatrix;
+import org.apache.commons.math3.linear.MatrixUtils;
 import org.apache.commons.math3.linear.RealMatrix;
 
+import java.sql.SQLOutput;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -10,6 +12,7 @@ public class Hill {
     public static final Scanner SCANNER = new Scanner(System.in);
 
     public static void main(String[] args) {
+
         char[] message = inputString("Ingrese un mensaje a cifrar:").toCharArray();
         int[][] key = keyChecker(inputString("Ingrese la llave:"), message.length);
 
@@ -20,40 +23,52 @@ public class Hill {
         System.out.println("\n");
         int[][] msg = codificate(message, key.length);
         System.out.println("Mensaje");
-        for (int[] c:msg){
+        for (int[] c : msg) {
             System.out.println(Arrays.toString(c));
         }
         System.out.println("\n");
-        int[][] newMessage = encrypt(msg,key);
+        int[][] newMessage = encrypt(msg, key);
         System.out.println("Mensaje Encriptado");
-        for (int[] c:newMessage){
+        for (int[] c : newMessage) {
             System.out.println(Arrays.toString(c));
         }
         System.out.println("\nNuevo Mensaje");
-        String mensaje = "";
+        StringBuilder mensaje = new StringBuilder();
         for (int i = 0; i < newMessage[i].length; i++) {
             for (int j = 0; j < newMessage.length; j++) {
-                mensaje += CHARACTERS_SPACE[newMessage[j][i]];
+                mensaje.append(CHARACTERS_SPACE[newMessage[j][i]]);
             }
         }
         System.out.println(mensaje);
 
+        System.out.println("------------{DECRYPT}-------------");
+        int[][] finalCrypt = decrypt(key, newMessage);
+        StringBuilder mensajeFinal = new StringBuilder();
+        System.out.println("Mensaje desencriptado \n");
+        for (int i = 0; i < finalCrypt[i].length; i++) {
+            for (int j = 0; j < finalCrypt.length; j++) {
+                mensajeFinal.append(CHARACTERS_SPACE[finalCrypt[j][i]]);
+            }
+        }
+        System.out.println(mensajeFinal);
+
+
     }
 
-    public static int[][] encrypt(int[][] message, int[][] key){
+    public static int[][] encrypt(int[][] message, int[][] key) {
 
         double[][] msgDouble = new double[message.length][message[0].length];
         double[][] keyDouble = new double[key.length][key[0].length];
 
-        for (int i = 0; i < msgDouble.length; i++){
-            for (int j = 0; j < msgDouble[i].length; j++){
+        for (int i = 0; i < msgDouble.length; i++) {
+            for (int j = 0; j < msgDouble[i].length; j++) {
 
                 msgDouble[i][j] = message[i][j];
 
             }
         }
-        for (int i = 0; i < keyDouble.length; i++){
-            for (int j = 0; j < keyDouble[i].length; j++){
+        for (int i = 0; i < keyDouble.length; i++) {
+            for (int j = 0; j < keyDouble[i].length; j++) {
 
                 keyDouble[i][j] = key[i][j];
 
@@ -67,53 +82,118 @@ public class Hill {
 
         int[][] newMsg = new int[message.length][message[0].length];
 
-        for (int i = 0; i < msgDouble.length; i++){
-            for (int j = 0; j < msgDouble[i].length; j++){
+        for (int i = 0; i < msgDouble.length; i++) {
+            for (int j = 0; j < msgDouble[i].length; j++) {
 
-                newMsg[i][j] = (int) (aux[i][j]%CHARACTERS_SPACE.length);
+                newMsg[i][j] = (int) (aux[i][j] % CHARACTERS_SPACE.length);
 
             }
         }
 
         return newMsg;
+    }
 
+    public static int[][] decrypt(int[][] k, int[][] encryptedMsg) {
+        double[][] kDouble = new double[k.length][k[0].length];
+        double[][] encryptedMessageDouble = new double[encryptedMsg.length][encryptedMsg[0].length];
+
+        for (int i = 0; i < kDouble.length; i++) {
+            for (int j = 0; j < kDouble[0].length; j++) {
+                kDouble[i][j] = k[i][j];
+            }
+        }
+
+        for (int i = 0; i < encryptedMessageDouble.length; i++) {
+            for (int j = 0; j < encryptedMessageDouble[0].length; j++) {
+                encryptedMessageDouble[i][j] = encryptedMsg[i][j];
+            }
+        }
+
+
+        RealMatrix kDoubleReal = new Array2DRowRealMatrix(kDouble);
+        RealMatrix kInverted = MatrixUtils.inverse(kDoubleReal);
+
+        double[][] kInvertedToPrimitive = kInverted.getData();
+
+        for (int i = 0; i < kInvertedToPrimitive.length; i++) {
+            for (int j = 0; j < kInvertedToPrimitive[0].length; j++) {
+                if (kInvertedToPrimitive[i][j] < 0) {
+                    kInvertedToPrimitive[i][j] = (kInvertedToPrimitive[i][j] % CHARACTERS_SPACE.length);
+                }
+            }
+        }
+
+        double determinanteOriginal = determinanteMatriz(k, k.length);
+        int inversoModular = 0;
+        for (int i = 0; i < determinanteOriginal; i++) {
+            if ((determinanteOriginal * i) % CHARACTERS_SPACE.length == 1) {
+                inversoModular = i;
+            }
+        }
+        //multiplicar a todos los elementos de la inversa
+        for (int i = 0; i < kInvertedToPrimitive.length; i++) {
+            for (int j = 0; j < kInvertedToPrimitive[0].length; j++) {
+                double aux1 = ((kInvertedToPrimitive[i][j] * inversoModular * determinanteOriginal) % CHARACTERS_SPACE.length);
+                if (aux1 < 0) {
+                    aux1 = aux1 * -1;
+                }
+                kInvertedToPrimitive[i][j] = Math.rint(aux1);
+            }
+        }
+
+        System.out.println(Arrays.deepToString(kInvertedToPrimitive));
+
+        //Hacer la desencriptacion
+        RealMatrix kInvertedToReal = new Array2DRowRealMatrix(kInvertedToPrimitive);
+
+        //Convertir la llave a real
+        RealMatrix msgConvertedToReal = new Array2DRowRealMatrix(encryptedMessageDouble);
+
+        //Multiplicar las matrices y aplicar el modulo
+        double[][] auxArray = kInvertedToReal.multiply(msgConvertedToReal).getData();
+        int[][] desencriptado = new int[encryptedMsg.length][encryptedMsg[0].length];
+
+        for (int i = 0; i < desencriptado.length; i++) {
+            for (int j = 0; j < desencriptado[i].length; j++) {
+                desencriptado[i][j] = (int) (auxArray[i][j] % CHARACTERS_SPACE.length);
+            }
+        }
+        return desencriptado;
     }
 
     public static int[][] codificate(char[] msgChar, int split) {
 
         int aux;
 
-        if (msgChar.length%split == 0){
-            aux = msgChar.length/split;
-        }else {
-            aux = (msgChar.length/split)+1;
+        if (msgChar.length % split == 0) {
+            aux = msgChar.length / split;
+        } else {
+            aux = (msgChar.length / split) + 1;
         }
 
         int[][] msgMatrix = new int[split][aux];
         int cont = 0;
-        int[] extras = new int[(aux*split)-msgChar.length];
+        int[] extras = new int[(aux * split) - msgChar.length];
         int exCont = 0;
 
-        for (int i = 0; i < msgMatrix[0].length; i++){
-            for (int j = 0; j < split; j++){
+        for (int i = 0; i < msgMatrix[0].length; i++) {
+            for (int j = 0; j < split; j++) {
                 int extra = 0;
-                if (cont >= msgChar.length){
-                    extra = (int) (Math.random()*CHARACTERS_SPACE.length);
+                if (cont >= msgChar.length) {
+                    extra = (int) (Math.random() * CHARACTERS_SPACE.length);
                     msgMatrix[j][i] = extra;
                     extras[exCont] = extra;
                     cont++;
                     exCont++;
-                }else{
+                } else {
                     msgMatrix[j][i] = getPosition(msgChar[cont]);
                     cont++;
                 }
             }
         }
-
-        System.out.println("Caracteres extras: "+Arrays.toString(extras));
+        System.out.println("Caracteres extras: " + Arrays.toString(extras));
 
         return msgMatrix;
-
     }
 
     private static int getPosition(final char c) {
@@ -187,7 +267,6 @@ public class Hill {
                         }
                     }
                 }
-
                 break;
             }
         }
